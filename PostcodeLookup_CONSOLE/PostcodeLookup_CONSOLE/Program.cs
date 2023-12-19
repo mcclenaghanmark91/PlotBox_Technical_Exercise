5b21142b9b3cb61bff1a562bf96f6b8604f5602a
Mark McClenaghan <mark.mcclenaghan@farmvetsystems.com>
2023-12-19 00:46:08 +0000

    Basic console App to call api

---
+++ PostcodeLookup_CONSOLE/PostcodeLookup_CONSOLE/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PostcodeLookup_CONSOLE
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static string path = "https://api.postcodes.io/postcodes/";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter a postcode to search:");
+            string postcode = Console.ReadLine();
+            string? data = await GetPostcodeInfo(postcode);
+
+            Console.WriteLine(data ?? "No data returned");
+        }
+
+        private static async Task<string?> GetPostcodeInfo(string postcode)
+        {
+            HttpResponseMessage response = await client.GetAsync(path + postcode);
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                return data;
+            }
+
+            return null;
+        }
+    }
+}
