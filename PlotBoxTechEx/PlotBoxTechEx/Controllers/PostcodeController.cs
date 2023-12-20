ffc50e834207988cdcf8a4b4b9ae8c9aa6ad0791
Mark McClenaghan <mark.mcclenaghan@farmvetsystems.com>
2023-12-20 11:35:25 +0000

    Initial Check in
    Initial check in of project files with some boilerplate code set up

---
+++ PlotBoxTechEx/PlotBoxTechEx/Controllers/PostcodeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlotBoxTechEx.Models;
+using System.Diagnostics;
+
+namespace PlotBoxTechEx.Controllers
+{
+    public class PostcodeController : Controller
+    {
+        private readonly ILogger<PostcodeController> _logger;
+
+        public PostcodeController(ILogger<PostcodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PostcodeData()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PlotBoxTechEx/PlotBoxTechEx/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlotBoxTechEx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
